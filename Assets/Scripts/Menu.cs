54cbee74247c77ce545a5db84628f52f370ca8dc
Harry <profawesum@gmail.com>
2021-08-29 12:01:47 +1200

    Main Menu Added

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Menu : MonoBehaviour
+{
+    public void singleplayerPushed() {
+        //load the singleplayer level
+        SceneManager.LoadScene("SinglePlayerLevel");
+    }
+    public void multiplayerPushed(){
+        //load the multiplayer level
+        SceneManager.LoadScene("MultiplayerLevel");
+    }
+    public void quitPushed(){
+        //quit the game
+        Application.Quit();
+    }
+}
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using Mirror;
+using UnityEngine.SceneManagement;
 
 namespace Mirror
 {
@@ -14,18 +15,21 @@ namespace Mirror
 
         [SyncVar] public int score;
 
-        //public void increaseScore(int scoreToIncrease)
-        //{
-        //    score += scoreToIncrease;
-        //}
-
         public void FixedUpdate()
         {
+            //set the score text
             scoreText.text = score.ToString();
 
+            //display a win message
             if (score == 5000) {
-                Time.timeScale = 0;
-                scoreText.text = score.ToString() + "  YOU WIN";
+                scoreText.text = score.ToString() + " YOU WIN";  
+            }
+
+            //make it so the player can return to the main menu
+            if (Input.GetKey(KeyCode.Escape))
+            {
+                //load the main menu
+                SceneManager.LoadScene(0);
             }
         }
     }
